8ddbe1001665930bbddc9a78f7e2836fd8e96bba
Royer Adames <royeraadames@gmail.com>
2024-03-06 13:29:09 -0400

    Where is the git connected to?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'new-angular-project-with-sass';
+  title = 'new-angular-project-with-sass!';
 }
