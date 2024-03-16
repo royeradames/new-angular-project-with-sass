c9b46c75822cf2f9a560259f1beed81c878138f1
royeradames <raadamesbonilla@pplweb.com>
2024-03-16 07:14:00 -0400

    update blog-preview-card: desktop UI

---
+++ src/app/shared/components/blog-preview-card/blog-preview-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-preview-card',
+  templateUrl: './blog-preview-card.component.html',
+  styleUrls: ['./blog-preview-card.component.scss']
+})
+export class BlogPreviewCardComponent {
+
+}
