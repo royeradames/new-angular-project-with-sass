411b712da456cd7955b2898050e472d9e2414082
royeradames <raadamesbonilla@pplweb.com>
2024-03-15 23:35:54 -0400

    update: setup prettier update

--- src/app/shared/components/card/qr-card.component.ts
+++ src/app/shared/components/card/qr-card.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import { Component, Input } from '@angular/core'
+import { NgOptimizedImage } from '@angular/common'
 
 export interface ICardInput {
   title: string
