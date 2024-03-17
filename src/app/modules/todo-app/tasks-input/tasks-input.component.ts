d943b56ad94633ac26a9bfcc8daa52aefd1953d5
royeradames <raadamesbonilla@pplweb.com>
2024-03-17 19:29:22 -0400

    update todo app: semantic and initial styles

---
+++ src/app/modules/todo-app/tasks-input/tasks-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-input',
+  templateUrl: './tasks-input.component.html',
+  styleUrls: ['./tasks-input.component.scss'],
+  standalone: true
+})
+export class TasksInputComponent {
+
+}
---
+++ src/app/modules/todo-app/todo-app.component.ts
@@ -0,0 +1,100 @@
+import { Component, Inject, Renderer2 } from '@angular/core'
+import { DOCUMENT, NgClass, NgForOf } from '@angular/common'
+import { ThemeService } from '../../core/services/theme.service'
+import { Subscription } from 'rxjs'
+import { IImage } from '../../core/interfaces/image.interface'
+import { TasksInputComponent } from './tasks-input/tasks-input.component'
+
+export interface ITodoList {
+  name: string
+  isCompleted: boolean
+}
+
+@Component({
+  selector: 'app-todo-app',
+  templateUrl: './todo-app.component.html',
+  styleUrls: ['./todo-app.component.scss'],
+  imports: [
+    NgClass,
+    NgForOf,
+    TasksInputComponent,
+  ],
+  standalone: true,
+})
+export class TodoAppComponent {
+  isDarkMode = this.themeService.isDarkMode.value
+  itemList: ITodoList[] = [
+    {
+      name: 'Complete online JavaScript course',
+      isCompleted: true,
+    },
+    {
+      name: 'Jog around the park 3x',
+      isCompleted: false,
+    },
+    {
+      name: '10 minutes meditation',
+      isCompleted: false,
+    },
+    {
+      name: 'Read for 1 hour',
+      isCompleted: false,
+    },
+    {
+      name: 'Pick up groceries',
+      isCompleted: false,
+    },
+    {
+      name: 'Complete Todo App on Frontend Mentor',
+      isCompleted: false,
+    },
+  ]
+  private themeSubscription!: Subscription
+
+  get itemLeft() {
+    const itemLeft = this.itemList.length + 1
+    return itemLeft
+  }
+
+  get themeModeImage() {
+    const lightThemeModeImage: IImage = {
+      href: 'assets/images/todo-app/icon-sun.svg',
+      alt: 'Sun',
+    }
+    const darkThemeModeImage: IImage = {
+      href: 'assets/images/todo-app/icon-moon.svg',
+      alt: 'Moon',
+    }
+    const themeModeImage = this.isDarkMode ? lightThemeModeImage : darkThemeModeImage
+    return themeModeImage
+  }
+
+  constructor(
+    private themeService: ThemeService,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
+  ) {
+  }
+
+  ngOnInit() {
+    this.themeSubscription = this.themeService.isDarkMode.subscribe(isDarkMode => {
+      this.isDarkMode = isDarkMode
+      if (isDarkMode) {
+        this.renderer.addClass(this.document.body, 'dark-mode')
+        this.renderer.removeClass(this.document.body, 'light-mode')
+        return
+      }
+      this.renderer.removeClass(this.document.body, 'dark-mode')
+      this.renderer.addClass(this.document.body, 'light-mode')
+    })
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe()
+  }
+
+  toggleThemeMode($event: MouseEvent, isDarkMode: boolean) {
+    $event.preventDefault()
+    this.themeService.isDarkMode.next(!isDarkMode)
+  }
+}
