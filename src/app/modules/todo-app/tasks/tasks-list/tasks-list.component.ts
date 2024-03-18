0bd7d316e233cd5bc626f40f8473167065297f25
royeradames <raadamesbonilla@pplweb.com>
2024-03-18 11:26:48 -0400

    update todo app: completed task list item UI

--- src/app/modules/todo-app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/modules/todo-app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
-import { NgForOf, NgOptimizedImage } from '@angular/common'
+import { NgClass, NgForOf, NgOptimizedImage } from '@angular/common'
 import { TodoAppService } from '../../todo-app.service'
+import { IImage } from '../../../../core/interfaces/image.interface'
 
 @Component({
   selector: 'app-tasks-list',
@@ -9,12 +10,25 @@ import { TodoAppService } from '../../todo-app.service'
   imports: [
     NgForOf,
     NgOptimizedImage,
+    NgClass,
   ],
   standalone: true,
 })
 export class TasksListComponent {
   itemList = this.todoAppService.itemList
 
+  getCheckBoxImage(isCompleted: boolean) {
+    const checkImage: IImage = {
+      href: 'assets/images/todo-app/checkbox-checked.svg',
+      alt: 'Checked box',
+    }
+    const uncheckImage: IImage = {
+      href: 'assets/images/todo-app/checkbox-unchecked.svg',
+      alt: 'unchecked box',
+    }
+    const getCheckBoxImage = isCompleted ? checkImage : uncheckImage
+    return getCheckBoxImage
+  }
   constructor(private todoAppService: TodoAppService) {
   }
 }
